579dfb61fffcd2cde838bd8a0fff69b43c9ec68f
Nate Silva <nate@natesilva.com>
2017-04-27 14:41:24 -0700

    add basic TypeScript definitions

---
+++ index.d.ts
@@ -0,0 +1,7 @@
+//
+// TypeScript definitions for this module.
+//
+
+export = sequelize_mysql_timestamp;
+declare function sequelize_mysql_timestamp(sequelize: any);
+declare namespace sequelize_mysql_timestamp {}
